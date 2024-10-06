17ede787a7fd9523531cb7fb0db1f633c9507481
Seth Bailey <104792463+Knabberpause@users.noreply.github.com>
2024-10-06 18:50:37 +0100

    initial setup with library imports and datafile processing started

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+from sense_hat import SenseHat #import library that allows us to access env sensors
+import time
+import csv
+
+sense = SenseHat()    #creates class instance on sensors so we can get the goods
+sense.clear()
+
+with open('data.csv', newline='') as file:
+    file_data = csv.reader(file, delimiter='', quotechar='/')
+    for row in file_data:
+        print(','.join(row))
+
+accel = sense.get_acceleration_raw()
+
+
+
 
