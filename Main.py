435c985e50c489f89457efefc95f4b1f4dac682d
Matthew <matthewenos002@gmail.com>
2025-02-21 14:57:03 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,25 +1,35 @@
 from sense_hat import SenseHat #import library that allows us to access env sensors
+from orbit import ISS # The simulator will only compile when using the 'orbit' library, even though the actual library that we are using is 'astro_pi_orbit
 import time
 import csv
-import logging
 import datetime
 
 sense = SenseHat()    #creates class instance on sensors so we can get the goods
 sense.clear()
-
+Velocity_Final = 0
 date = datetime.datetime.now()
-filename_datalog = f'data_{date.strftime('%d')}{date.strftime('%m')}{date.strftime('%y')}{date.strftime('%h')}{date.strftime('%M')}{date.strftime('%S')}'
-print(filename_datalog)
+iss = ISS()
 
-with open(f'{filename_datalog}.csv', newline='') as envdata:
-    file_data = csv.reader(envdata, delimiter='', quotechar='/')
-    for row in file_data:
-        print(','.join(row))
 
-accel = sense.get_acceleration_raw()
-ac_x = accel['x']
-ac_y = accel['y']
-ac_z = accel['z']
+with open("data.csv", "w") as f:
+    writer = csv.writer(f)
+    header = ['ac_X', 'ac_Y', 'ac_Z', 'lat', 'lon', 'elev']
+    writer.writerow(header)
+    for i in range(570): #once every second for 570 seconds (9.5 minutes)
+        accel = sense.get_accelerometer_raw()
+        ac_x = accel['x']
+        ac_y = accel['y']
+        ac_z = accel['z']
+        coordinates = iss.coordinates()
+        print(coordinates)
+        data = [ac_x, ac_y, ac_z, coordinates.longitude.signed_dms(), coordinates.longitude.signed_dms(), coordinates.elevation]
+        #Velocity_angular = sense._read_imu()
+        #print(Velocity_angular)
+        print(ac_x, ac_y, ac_z)
+        writer.writerow(data)
+        time.sleep(1) # Wait one second
+
+
 
 
 
